89d265ff28cd072d7dfe19ac13cfbe0455536040
kree8vestudio <fanbase0007@gmail.com>
2026-02-18 13:28:50 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,28 +1,35 @@
-// SET YOUR LAUNCH DATE HERE
+// LAUNCH DATE
 const launchDate = new Date("April 1, 2026 00:00:00").getTime();
 
-const timer = setInterval(function(){
+// COUNTDOWN
+setInterval(function(){
 
 const now = new Date().getTime();
 
 const distance = launchDate - now;
 
 const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+const hours = Math.floor((distance / (1000 * 60 * 60)) % 24);
+const minutes = Math.floor((distance / (1000 * 60)) % 60);
+const seconds = Math.floor((distance / 1000) % 60);
 
 document.getElementById("days").innerText = days;
 document.getElementById("hours").innerText = hours;
 document.getElementById("minutes").innerText = minutes;
 document.getElementById("seconds").innerText = seconds;
 
-if(distance < 0){
+}, 1000);
 
-clearInterval(timer);
 
-document.querySelector(".coming").innerText = "LIVE NOW";
+// SAVE EMAIL
+function saveEmail(event){
+
+event.preventDefault();
+
+let email = document.getElementById("emailInput").value;
+
+localStorage.setItem("fleuxe_email", email);
+
+alert("You will be notified on launch!");
 
 }
-
-}, 1000);
